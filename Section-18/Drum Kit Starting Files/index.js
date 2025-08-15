edb7e7c6d855e7abcd5465eb8f1a78f8047564ab
Pathfinder132 <uttejmekala@gmail.com>
2025-08-15 14:10:14 +0530

    made changes to the webpage by adding few more features

--- Section-18/Drum Kit Starting Files/index.js
+++ Section-18/Drum Kit Starting Files/index.js
@@ -1,6 +1,58 @@
-for(var i = 0;i<document.querySelectorAll(".drum").length;i++){
-document.querySelectorAll(".drum")[i].addEventListener("click",
-    function () {
-    alert("I got clicked!");
+document.addEventListener("keyup", function(E){
+    let temp=E.key;
+    let l = document.querySelector("."+temp);
+    l.style.color="white";
+    setTimeout(function(){
+        l.style.color = "#DA0463";
+    }, 100);
+    makeSound(E.key);   
+    
+});
+function makeSound(key){
+    let sym = document.querySelector("."+key+" img");
+    sym.style.opacity=1;
+    switch (key) {
+        case "w":
+            var aud = new Audio("sounds/tom-1.mp3");
+            aud.play();
+            break;
+        case "a":
+            var aud = new Audio("sounds/tom-2.mp3");
+            aud.play();
+            break;
+        case "s":
+            var aud = new Audio("sounds/tom-3.mp3");
+            aud.play();
+            break;
+        case "d":
+            var aud = new Audio("sounds/tom-4.mp3");
+            aud.play();
+            break;
+        case "j":
+            var aud = new Audio("sounds/snare.mp3");
+            aud.play();
+            break;
+        case "k":
+            var aud = new Audio("sounds/kick-bass.mp3");
+            aud.play();
+            break;
+        case "l":
+            var aud = new Audio("sounds/crash.mp3");
+            aud.play();
+            break;
+        
+        default:
+            break;
+    }
+    setTimeout(function(){
+        sym.style.opacity=0;
+    },100);
+    
+}
+for(let i = 0;i<document.querySelectorAll(".drum").length;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",
+    function (){
+    let btn = this.innerHTML;
+    makeSound(btn);
 });
 }
